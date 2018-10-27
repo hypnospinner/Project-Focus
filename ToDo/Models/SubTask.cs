8b2d1cd32e665341b963cefac10d195fd16227ef
Elijah Katun <hypnospinner@gmail.com>
2018-10-27 12:34:05 +0300

    basic task setup

--- ToDo/Models/SubTask.cs
+++ ToDo/Models/SubTask.cs
@@ -8,5 +8,6 @@ namespace ToDo.Models
 {
     class SubTask
     {
+
     }
 }
--- ToDo/Models/Task.cs
+++ ToDo/Models/Task.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ToDo.Models
 {
-    class Task
+    class Task : INotifyPropertyChanged
     {
+        private string _taskDescription;        // name of task        
+        private string _taskNote;               // some notes for task
+        private bool _isDone;                   // weather wask is done or note
+        public string TaskDescription
+        {
+            get => _taskDescription;
+            set
+            {
+                _taskDescription = value;
+                OnPropertyChange("Task Description");
+            }
+        }
+        public string TaskNote
+        {
+            get => _taskNote;
+            set
+            {
+                _taskNote = value;
+                OnPropertyChange("TaskNote");
+            }
+        }
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                _isDone = value;
+                OnPropertyChange("IsDone");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChange([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
