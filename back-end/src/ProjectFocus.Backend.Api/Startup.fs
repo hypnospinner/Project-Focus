de39c8120edfd9d02d6d5d6313ce1114ef1163e2
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2018-12-08 17:15:12 +0300

    minor fix

--- back-end/src/ProjectFocus.Backend.Api/Startup.fs
+++ back-end/src/ProjectFocus.Backend.Api/Startup.fs
@@ -1,12 +1,8 @@
 namespace ProjectFocus.Backend.Api
 
 open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy;
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
@@ -21,7 +17,7 @@ type Startup private () =
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1) |> ignore
-        services |> Service.addRabbitMq this.Configuration |> ignore
+        services |> Service.addRabbitMq this.Configuration 
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
